5560bef8c40c417db4943f1442f9bac85f76c71a
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2025-12-11 18:38:02 +0530

    small change

--- App Source Files/Insurance.py
+++ App Source Files/Insurance.py
@@ -4,6 +4,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 
+
 df = pd.read_csv(r"C:\Portfoilo Projects\Medical-Insurance-Prediction\Data\insurance.csv")
 
 df_1 = df.drop(columns= 'sex', axis = 1)
