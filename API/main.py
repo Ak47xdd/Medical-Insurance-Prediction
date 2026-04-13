b5dc2e38798afe299ad33c8db81b26109f18a501
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-04-13 16:07:23 +0530

    API

--- API/main.py
+++ API/main.py
@@ -1,30 +1,35 @@
 from fastapi import FastAPI
+from fastmcp import FastMCP
 import pandas as pd
 
 import MedPred as med
 
-def get_data():
-    age = med.age_data
-    bmi = med.bmi_data
-    ins = med.ins_data
+app = FastAPI(title="Database Management API - MedPred.AI")
 
-    return age, bmi, ins
+# def get_data():
+#     age = med.age_data
+#     bmi = med.bmi_data
+#     ins = med.ins_data
 
-age, bmi, ins = get_data()
+#     return age, bmi, ins
 
-get_data()
+@app.post("/data")
+def get_val():
+    age, bmi, ins = med.get_data()
 
-pred_val = ins
-pred_val = [round(x, 4) for x in pred_val]
+    pred_val = ins
+    pred_val = [round(x, 4) for x in pred_val]
 
-# print(age)
-# print(bmi)
-# print(pred_val)
+    # print(age)
+    # print(bmi)
+    # print(pred_val)
 
-rows = []
-for a, b, c in zip(age, bmi, pred_val):
-    rows.append({'age': a, 'bmi': b, 'charges': c})
-    
-new_data = pd.DataFrame(rows)
+    rows = []
+    for a, b, c in zip(age, bmi, pred_val):
+        rows.append({'age': a, 'bmi': b, 'charges': c})
+        
+    new_data = pd.DataFrame(rows)
 
-new_data.to_csv("new_data.csv", mode='a', index=False, header=False)
+    new_data.to_csv("new_data.csv", mode='a', index=False, header=False)
+
+# get_val()
